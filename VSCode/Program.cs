97db5f018558cd6984f0ab75c53e4e201c1705c4
Ihzam Chaidir Aldy <ihzam.aldy27@gmail.com>
2022-10-04 07:02:31 +0700

    Exercise 2
    ConvertTime

---
+++ VSCode/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masukkan Waktu (hh:mm:ssAM / hh:mm:ssPM) :");
+            var s = Console.ReadLine();
+            Console.WriteLine(ConvertTime(s));
+            Console.ReadKey();
+        }
+
+        static string ConvertTime(string s){
+        return DateTime.Parse(s).ToString("HH:mm:ss");
+        }
+    }
+}
